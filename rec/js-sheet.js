4c9a519eeee09ec9463e7fa3ad043d7e39d2b28f
Wasib <wasibulislam8984@gmail.com>
2024-02-26 13:39:01 +0600

    Seat section

--- rec/js-sheet.js
+++ rec/js-sheet.js
@@ -1,3 +1,29 @@
 function coupon(couponCode){
     console.log(couponCode);
 }
+
+
+
+function buttonFun(button){
+    buttonId=button.id
+    totalSeatsCont= document.getElementById("seats")
+    totalSeatsText= document.getElementById("seats").innerText
+    totalSeats= parseInt(totalSeatsText)
+
+    const buttonClassList = button.classList;
+
+    if (buttonClassList.contains('bg-[#F7F8F8]')  ) {
+        buttonClassList.remove('bg-[#F7F8F8]');
+        buttonClassList.add("bg-[#1DD100]");
+        nowTotalSeats = totalSeats -1;
+    }
+    else{
+        buttonClassList.add('bg-[#F7F8F8]');
+        buttonClassList.remove("bg-[#1DD100]");
+        nowTotalSeats = totalSeats +1;
+    }
+    totalSeatsCont.innerText = nowTotalSeats
+    
+    
+
+}
